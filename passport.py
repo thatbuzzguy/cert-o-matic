f084c9700cbe307813206b7d91b1bc24d59a42e7
Ryan Brown <rdavidbrown@live.com>
2016-06-11 19:20:44 -0700

    rollup

--- passport.py
+++ passport.py
@@ -4,8 +4,6 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
-from flask import Flask, request, jsonify, Response
 import datetime
 import uuid
 
@@ -20,38 +18,44 @@ def gen_rsa_public_key(private_key):
     public_key = private_key.public_key()
     return(public_key)
 
-def gen_cn(cn):
-    subject = x509.Name([
+def build_subject_name(common_name):
+    subject_name = x509.Name([
         x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
         x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"CA"),
         x509.NameAttribute(NameOID.LOCALITY_NAME, u"West Sacramento"),
         x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"Personal"),
-        x509.NameAttribute(NameOID.COMMON_NAME, cn),
+        x509.NameAttribute(NameOID.COMMON_NAME, common_name),
         ])
-    return subject
+    return subject_name
 
 def gen_csr(private_key, subject):
     csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name(subject)).sign(private_key, hashes.SHA256(), default_backend())
     return(csr)
 
-def gen_cert(private_key, public_key, csr):
+def gen_root_cert(private_key, public_key):
     builder = x509.CertificateBuilder()
-    builder = builder.subject_name(x509.Name([
-        x509.NameAttribute(NameOID.COMMON_NAME, u'cryptography.io'),
-        ]))
-    builder = builder.issuer_name(x509.Name([
-        x509.NameAttribute(NameOID.COMMON_NAME, u'cryptography.io'),
-        ]))
-    builder = builder.not_valid_before(datetime.datetime.today() - one_day)
-    builder = builder.not_valid_after(datetime.datetime(2018, 8, 2))
+    builder = builder.subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, u'Test Root 1'),]))
+    builder = builder.issuer_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, u'Passport Development Lab'),]))
+    builder = builder.not_valid_before(datetime.datetime.today())
+    builder = builder.not_valid_after(datetime.datetime.today() + one_day)
     builder = builder.serial_number(int(uuid.uuid4()))
     builder = builder.public_key(public_key)
-    builder = builder.add_extension(
-        x509.BasicConstraints(ca=False, path_length=None), critical=True,
-    )
-    certificate = builder.sign(
-        private_key=private_key, algorithm=hashes.SHA256(),
-        backend=default_backend())
+    builder = builder.add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True,)
+    certificate = builder.sign(private_key=private_key, algorithm=hashes.SHA256(),backend=default_backend())
+    
+    return(certificate)
+
+def gen_child_cert(private_key, public_key, csr, subject_name):
+    builder = x509.CertificateBuilder()
+    builder = builder.subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, cn),]))
+    builder = builder.issuer_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, u'Passport Development Lab'),]))
+    builder = builder.not_valid_before(datetime.datetime.today())
+    builder = builder.not_valid_after(datetime.datetime.today() + one_day)
+    builder = builder.serial_number(int(uuid.uuid4()))
+    builder = builder.public_key(public_key)
+    builder = builder.add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True,)
+    certificate = builder.sign(private_key=private_key, algorithm=hashes.SHA256(),backend=default_backend())
+    
     return(certificate)
         
 
@@ -70,26 +74,11 @@ def pem_encode_csr(csr):
     return(pem)
 
 
+private_key = gen_rsa_private_key(2048)
+public_key = gen_rsa_public_key(private_key)
+csr = gen_csr(private_key, common_name)
 
 
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def version():
-    return(appversion)
-
-@app.route('/test')
-def test():
-    private_key = gen_rsa_private_key(2048)
-    public_key = gen_rsa_public_key(private_key)
     
-    subject = gen_cn('test')
-    csr = gen_csr(private_key, subject)
 
-    pem_csr = pem_encode_csr(csr)
-    pem_public_key = pem_encode_public_key(public_key)
-    
-    resp = Response(response=pem_public_key, status=200, mimetype="application/json")
-    return(resp)
+
